3a4c8057e223094061f3ef6ffb784cf96d4ed57b
Femboy Jophiel <104801958+femboyjophiel@users.noreply.github.com>
2023-09-24 03:47:22 -0400

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,10 +1,10 @@
-[
-    "Example Site 1":{
+{
+    "Example Site 1": {
         "link": "https://example.com",
-        "verified": 09/24/2023
+        "verified": "09/24/2023"
     },
-    "Example Site 2":{
+    "Example Site 2": {
         "link": "https://anotherexample.com",
-        "verified": 09/24/2023
+        "verified": "09/24/2023"
     }
-]
+}
