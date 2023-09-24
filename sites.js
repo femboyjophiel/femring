0ee44310524a366c6f35ef6735feae71c88ef8e0
Femboy Jophiel <104801958+femboyjophiel@users.noreply.github.com>
2023-09-24 03:04:20 -0400

    Create sites.js

---
+++ sites.js
@@ -0,0 +1,13 @@
+[
+    {
+        "name": "Example Site 1",
+        "link": "https://example.com",
+        "verified": true
+    },
+    {
+        "name": "Another Example Site",
+        "link": "https://anotherexample.com",
+        "verified": false
+    }
+    // Add more sites here
+]
